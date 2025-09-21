59836ce9614907e5510d8f66a5f7106e4a99a77e
ThiagoFTelles <atendimento.telles@gmail.com>
2025-09-21 15:35:40 -0300

    feat: map setup

---
+++ components/app/map.client.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { CENTER_BR } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const styleLight = "https://tiles.openfreemap.org/styles/liberty";
+const styleDark = "/styles/dark.json";
+const style = computed(() => colorMode.value === "dark" ? styleDark : styleLight);
+const zoom = 2;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_BR"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
---
+++ lib/constants.ts
@@ -0,0 +1,1 @@
+export const CENTER_BR = [-40.294090, -20.284481];
